8efdc23420300ad11757686454d38599a812b15e
jaxongir <j.niyazmetov@newuu.uz>
2025-05-20 01:20:06 +0500

    final form

--- sheets.py
+++ sheets.py
@@ -1,13 +1,24 @@
+import os
+import json
+import base64
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
-from config import GOOGLE_SHEET_NAME
+
 
 def get_sheet():
-    scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-    creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
-    client = gspread.authorize(creds)
-    return client.open(GOOGLE_SHEET_NAME).sheet1
+    scope = [
+        'https://spreadsheets.google.com/feeds',
+        'https://www.googleapis.com/auth/drive'
+    ]
+    b64_creds = os.getenv("GOOGLE_CREDS_B64")
 
-def append_data(name, age, phone):
-    sheet = get_sheet()
-    sheet.append_row([name, age, phone])
+    if not b64_creds:
+        raise Exception("GOOGLE_CREDS_B64 environment variable not found.")
+
+    creds_dict = json.loads(base64.b64decode(b64_creds))
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
+    client = gspread.authorize(creds)
+
+    sheet_name = os.getenv('GOOGLE_SHEET_NAME')
+    sheet = client.open(sheet_name).sheet1
+    return sheet
